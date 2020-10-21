c81c4f0cdeb59d13aee69e430b5e61fbd6121aea
arghavan99 <55342129rez>
2020-10-21 16:55:06 +0330

    gamma code

--- compression.py
+++ compression.py
@@ -1,6 +1,7 @@
 import sys
 
 
+####### Variable length encoding:
 def VB_encode_number(num):
     arr = bytearray()
     while True:
@@ -28,16 +29,48 @@ def VB_decode(bytestream):
     n = 0
     for b in bytestream:
         if b < 128:
-            n = n*128 + b
+            n = n * 128 + b
         else:
-            n = n*128 + (b-128)
+            n = n * 128 + (b - 128)
             numbers.append(n)
             n = 0
     return numbers
 
 
+####### Gamma encoding:
+def gamma_encode_number(num):
+    binary_num = format(num, 'b')
+    binary_num = binary_num[1:]
+    length = '1' * (len(binary_num)) + '0'
+    res = length + binary_num
+    return res
+
+
+def gamma_encode_all(numbers):
+    bytestream = ''
+    for num in numbers:
+        x = gamma_encode_number(num)
+        bytestream += x
+    return bytestream
+
+
+def gamma_decode(bits):
+    numbers = []
+    i = 0
+    last = 0
+    while i < len(bits):
+        if bits[i] == '0':
+            length = i - last
+            offset = '1' + bits[i + 1:i + length + 1]
+            numbers += [int(offset, 2)]
+            i += length + 1
+            last = i
+        else:
+            i += 1
+    return numbers
+
 # t = VB_encode_all([824, 5])
 # print(VB_decode(t))
 
-
-
+# t = gamma_encode_all([1025, 9, 4, 3])
+# print(gamma_decode(t))
