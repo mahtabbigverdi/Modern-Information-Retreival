6a1c109414001c7ad512e7b681aec3dd4adeb295
mahtabbigverdi <bigverdi.1377@gmail.com>
2020-10-21 23:52:26 +0330

    debug persian preprocess

--- preprocess_fa.py
+++ preprocess_fa.py
@@ -15,26 +15,34 @@ def preprocess_farsi (text):
     ### Remove punctuation
     new_text = re.sub(r'[^\w\s]', ' ', new_text)
 
-    ### Remove numbers
-    new_text = re.sub(r'[۱۲۳۴۵۶۷۸۹۰]', ' ', new_text)
+
+
+
 
     normalizer = hazm.Normalizer(remove_extra_spaces=True, persian_style=True, persian_numbers=True, remove_diacritics=True, affix_spacing=True, token_based=False, punctuation_spacing=True)
     new_text = normalizer.normalize(new_text)
 
+    ### Remove numbers
+    new_text = re.sub(r'[۱۲۳۴۵۶۷۸۹۰]', ' ', new_text)
+
+
     ### Not in HAZM
-    new_text = new_text.replace('گی','ه')
+    # new_text = new_text.replace('گی','ه')
 
     tokens = hazm.word_tokenize(new_text)
     stemmer = hazm.Stemmer()
     lemmatizer = hazm.Lemmatizer()
 
 
+    tokens = [word.replace('\u200c', '‌') for word in tokens ]
+
+
     tokens = [stemmer.stem(word) for word in tokens]
     tokens = [lemmatizer.lemmatize(word) for word in tokens]
 
     tokens = [word for word in tokens if word != '' ]
-    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-    print(tokens)
+
+    return tokens
 
 
 
@@ -47,5 +55,6 @@ def preprocess_farsi (text):
 wikis = pd.read_csv('data/out.csv')
 titles = list(wikis['title'])
 texts = list(wikis['text'])
-preprocess_farsi(texts[0])
+preprocess_farsi(texts[8])
+
 
