b8fcaa285df46dbf7736ad1d33ef8cf960216c53
mahtabbigverdi <bigverdi.1377@gmail.com>
2020-10-26 15:19:06 +0330

    tf-idf debug

--- scoring.py
+++ scoring.py
@@ -13,14 +13,23 @@ def get_idf (index, term, total_number_docs):
     return np.log10(total_number_docs/df)
 
 def get_normalized (vector):
-    out = np.sqrt(sum([x*x for x in vector]))
-    if out == 0 :
-        return 1
-    return out
+    return np.sqrt(sum([x*x for x in vector]))
+
+
+def lenght_doc(index, doc_id, total_number_of_docs):
+    out = []
+    for term in index.keys():
+        if doc_id in index[term]:
+            w_td = get_logarithmic_tf(index, term, doc_id) * get_idf(index, term, total_number_of_docs)
+            out.append(w_td)
+    return get_normalized(out)
+
+
 
 def score_tfidf(index, query, total_number_of_docs):
-    doc_vectors = [[0] for i in range(total_number_of_docs)]
+    scores = [0 for i in range(total_number_of_docs)]
     query_vector = []
+
     my_query = list(set(query))
 
 
@@ -31,17 +40,24 @@ def score_tfidf(index, query, total_number_of_docs):
 
         for doc in index[term].keys():
             w_td = get_idf(index, term, total_number_of_docs) * get_logarithmic_tf(index, term, doc)
-            doc_vectors[doc].append(w_td)
+            scores[doc]+=(w_td*w_tq)
 
 
-    lenghts = [get_normalized(vector) for vector in doc_vectors]
-    doc_vectors = [list(np.array(doc_vectors[i])/lenghts[i]) for i in range(total_number_of_docs)]
-    query_vector = np.array(query_vector) / get_normalized(query_vector)
-    doc_vectors = np.array(doc_vectors)
+    lengths = [lenght_doc(index, doc_id, total_number_of_docs) for doc_id in range(total_number_of_docs)]
 
-    scores = doc_vectors * query_vector
-    scores = np.sum (scores, axis=1)
-    top_docs = np.argsort(scores)[-10: ]
+    query_size = get_normalized(query_vector)
+
+    scores = np.array(scores)
+    lengths =np.array(lengths)
+
+    scores /= lengths
+    scores /= query_size
+
+    top_docs = np.argsort(scores)[-10: ][::-1]
     return top_docs
 
 
+index = (load_index('pos_index' + '.pkl'))
+query =['i','am','good', 'girl']
+
+print(score_tfidf(index, query, 3))
