a091168afe5616457f84100581fe1d308e0fa01f
arghavan99 <55342129rez>
2020-12-18 10:31:29 +0330

    knn

---
+++ KNN.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import math
+import operator
+import numpy as np
+
+
+class KNN:
+    def __init__(self):
+        self.k = 1
+        self.X = None
+        self.Y = None
+        self.columns = None
+
+    def fit(self, X, Y):
+        self.X = X
+        self.Y = Y
+        self.columns = X.columns
+
+    def sample_distance(self, train_sample, test_sample):
+        diff = test_sample - train_sample
+        dist = np.power(diff, 2)
+        return math.sqrt(dist)
+
+    def distances(self, test):
+        dist = []
+        for i in range(len(self.X)):
+            dist.append(self.sample_distance(self.X.iloc[i], test))
+        return dist
+
+    def predict_sample(self, test, k):
+        self.k = k
+        predict = []
+        for j in range(len(test)):
+            t = test.iloc[j]
+            dist = self.distances(t)
+            labels = []
+            for i in range(len(dist)):
+                item = dist[i]
+                labels.append([item, self.Y[i]])
+            s = sorted(labels, key=lambda x: x[0])
+            s = s[:self.k]
+            classes = {}
+            for i in range(self.k):
+                vote = s[i][1]
+                if classes.__contains__(vote):
+                    classes[vote] += 1
+                else:
+                    classes.update({vote: 1})
+            predict.append(max(classes.items(), key=operator.itemgetter(1))[0])
+        return predict
+
+
